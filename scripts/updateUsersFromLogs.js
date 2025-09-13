0f1fd796338298d18555393f29eb2e558a4204f2
pool2win <78231905+pool2win@users.noreply.github.com>
2025-09-13 16:03:11 +0200

    Update users from log files
    - add a new script file
    - add a new package script to run the above script
    - we read the user data directly from fs, so we don't need to deal with only updating the registered users
    - we show data for all users on the pool

--- scripts/updateUsersFromLogs.js
+++ scripts/updateUsersFromLogs.js
@@ -64,7 +64,11 @@ async function updateWorker(address, workerData) {
     return;
   }
 
-  const workerName = workerData.workername; // Show full worker name
+  const workerName = workerData.workername.includes('.')
+    ? workerData.workername.split('.')[1]
+    : workerData.workername.includes('_')
+      ? workerData.workername.split('_')[1]
+      : '';
 
   const worker = await prisma.worker.upsert({
     where: {
